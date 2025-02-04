cfd6938ea78017dc411051a8013b575503749227
Brad Richardson <bradrichardsonmail@gmail.com>
2025-02-03 22:54:42 -0500

    Format

--- nlp.py
+++ nlp.py
@@ -5,6 +5,7 @@ import argparse
 import pathlib
 import csv
 
+
 def create_lv_prompt(name_to_test: str) -> str:
     return f"""
     Assess whether the crowd-sourced map feature from OpenStreetMap with the name "{name_to_test}" is correctly labeled as specifically for English map users.
@@ -14,6 +15,7 @@ def create_lv_prompt(name_to_test: str) -> str:
     An example of `no_issue` would be "Germany", whereas "Deutschland" would be `invalid`. Default to `no_issue` when unsure.
     """
 
+
 def create_vandalism_prompt(name_to_test: str) -> str:
     return f"""
     Assess whether the crowd-sourced map feature from OpenStreetMap with the name "{name_to_test}" is vandalism or profane or would not be used to describe real world features like roads, buildings, places or POIs. Note that these can be in any world language, not just English.
@@ -21,12 +23,13 @@ def create_vandalism_prompt(name_to_test: str) -> str:
     Please provide a label of either "vandalism" or "regular", rate it between 0.0 and 1.0, where 0.0 means a name that has no chance of being vandalism and could appear on e.g. a car nav system and 1.0 means this is very likely an invalid or vandalized name in this context. Also a brief paragraph describing reasoning.
     """
 
+
 def evaluate_prompts(model_name, csv_path, eval_type):
     # Initialize local Ollama client
     client = Client()
 
     # Read CSV file
-    reader = csv.DictReader(open(csv_path), delimiter=',')
+    reader = csv.DictReader(open(csv_path), delimiter=",")
 
     # Initialize results tracking
     correct = 0
@@ -72,7 +75,7 @@ def evaluate_prompts(model_name, csv_path, eval_type):
             else:
                 # print(f"Mismatch for {row['name_tag']} | expected {row['label']} | score {response.get("risk_score")}")
                 pass
-            
+
             response["name_tag"] = row.get("name_tag")
             response["expected"] = row.get("label")
             results.append(response)
@@ -85,11 +88,10 @@ def evaluate_prompts(model_name, csv_path, eval_type):
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(
-                    prog='Overture NLP')
-    
+    parser = argparse.ArgumentParser(prog="Overture NLP")
+
     parser.add_argument("filename")
-    parser.add_argument("--type", choices=['language-validation', 'vandalism'])
+    parser.add_argument("--type", choices=["language-validation", "vandalism"])
     parser.add_argument("--model")
     parser.add_argument("--all-models", action="store_true")
     parser.add_argument("--log-dir", default="./logs")
@@ -99,10 +101,14 @@ if __name__ == "__main__":
     if not args.all_models and not args.model:
         print("Need --model or --all-models set")
         exit(1)
-    
+
     pathlib.Path(args.log_dir).mkdir(parents=True, exist_ok=True)
 
-    models = ["llama3.2:1b", "llama3.2:3b", "phi4:14b", "deepseek-r1:14b"] if args.all_models else [args.model]
+    models = (
+        ["llama3.2:1b", "llama3.2:3b", "phi4:14b", "deepseek-r1:14b"]
+        if args.all_models
+        else [args.model]
+    )
 
     print(f"Evaluating {args.type} for models {models}")
     for model in models:
@@ -114,6 +120,6 @@ if __name__ == "__main__":
 
         print(f"Overall Accuracy: {accuracy:.2%}")
         print(f"Total time (min): {(t1-t0)/60.0:.2}")
-        model_log_path = f"{args.log_dir}/vandalism_{model.replace(":", "")}.json"
-        with open(model_log_path, 'w') as f:
+        model_log_path = f"{args.log_dir}/vandalism_{model.replace(':', '_')}.json"
+        with open(model_log_path, "w") as f:
             json.dump(results, f, indent=4)
