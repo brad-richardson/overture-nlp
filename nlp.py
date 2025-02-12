b26a4e00dc9c3e436f8e150ab6146060dd786b58
Brad Richardson <bradrichardson@meta.com>
2025-02-12 15:34:52 -0500

    Fix prompt generation, add updated corpus

--- nlp.py
+++ nlp.py
@@ -4,6 +4,7 @@ import json
 import pathlib
 import time
 from datetime import datetime
+from typing import Optional, Tuple
 
 from llama_cpp import Llama
 
@@ -73,17 +74,21 @@ def create_vandalism_system_prompt() -> str:
     Note that these names may be in any language, not just English. During assessment, please consider diverse political, lingual, socioconomic and geographic viewpoints in the evaluation.
 
     Please provide the following:
-    - a label of "profanity", "vandalism", "suspicious" or "no_issue"
+    - a label of "vandalism", "profanity", "suspicious" or "no_issue"
     - a risk score between 0.0 and 1.0, where 0.0 indicates the name has no chance of being problematic and could appear on e.g. a car nav system and 1.0 means definitely an issue and likely to be offensive or unwelcome
-    - a brief (<=2 sentences) reasoning for the rating
+    - a brief (2 sentences) reasoning for the rating
 
-    Additional prompt context may be provided, such as language code, previous name and the feature's OSM tags as JSON. These fields should be used as context only, and not directly considered for the output label and score.
+    Additional prompt context may be provided, such as tag key (indicating language or special name type), previous name and the feature's OSM tags as JSON. These fields should be used as context only, and not directly considered for the output label and score.
     """
 
 
 def evaluate_vandalism(
-    llm, new_name, old_name, target_language_code, filtered_tags_json
-):
+    llm,
+    new_name: Optional[str],
+    old_name: Optional[str],
+    name_key: Optional[str],
+    filtered_tags_json: Optional[str],
+) -> Optional[dict]:
     system_prompt = create_vandalism_system_prompt()
 
     # Build user prompt
@@ -91,14 +96,14 @@ def evaluate_vandalism(
         user_prompt = "Name was removed\n"
     else:
         user_prompt = f"New name: '{new_name}'\n"
-    if target_language_code:
-        user_prompt += f"Target language code: '{target_language_code}'\n"
     if old_name:
         user_prompt += f"Old name: '{old_name}'\n"
+    if name_key:
+        user_prompt += f"OSM name key: '{name_key}'\n"
     if filtered_tags_json:
-        user_prompt += f"Tags: {filtered_tags_json}\n"
+        user_prompt += f"OSM tags: {filtered_tags_json}\n"
 
-    allowed_labels = ["profanity", "vandalism", "suspicious", "no_issue"]
+    allowed_labels = ["vandalism", "profanity", "suspicious", "no_issue"]
     json_schema = {
         "type": "object",
         "properties": {
@@ -109,6 +114,7 @@ def evaluate_vandalism(
         "required": ["label", "risk_score", "reason"],
     }
 
+    generated = None
     try:
         generated = llm.create_chat_completion(
             messages=[
@@ -125,24 +131,28 @@ def evaluate_vandalism(
             # Low temperature for predictability, high top_p for diversity
             temperature=0.2,
             top_p=0.9,
-            max_tokens=2048,
         )
 
-        response = json.loads(generated.get("choices")[0].get("message").get("content"))
+        raw_response = generated.get("choices")[0].get("message").get("content")
+        response = json.loads(raw_response)
 
+        response["prompt"] = user_prompt
         response["new_name"] = new_name
         response["old_name"] = old_name
         return response
     except Exception as e:
         print(f"Error processing row: {e}")
+        if generated:
+            print(generated)
         return None
 
 
-def evaluate_prompts(csv_path, eval_type):
+def evaluate_prompts(eval_type: str, csv_path: str) -> Tuple[list, float]:
     print(f"Loading model {MODEL_FILENAME}")
     llm = Llama.from_pretrained(
         repo_id=MODEL_REPO,
         filename=MODEL_FILENAME,
+        n_ctx=2048,
         verbose=False,
     )
     print(f"Loaded model {MODEL_FILENAME}")
@@ -169,19 +179,30 @@ def evaluate_prompts(csv_path, eval_type):
             if eval_type == "language-validation":
                 result = evaluate_language_validation(llm)
             elif eval_type == "vandalism":
-                # TODO - remaining params
-                result = evaluate_vandalism(llm, row.get("name_tag"), "", "", "")
-                if result:
-                    result["expected"] = row.get("label")
+                result = evaluate_vandalism(
+                    llm,
+                    new_name=row.get("new_name"),
+                    old_name=row.get("old_name"),
+                    name_key=row.get("tag"),
+                    filtered_tags_json=row.get("filtered_tags"),
+                )
             else:
                 raise ValueError(f"Unknown eval type: {eval_type}")
 
             if result:
                 results.append(result)
-                if result["label"] != "no_issue" and result["expected"] != "regular":
-                    correct += 1
-                elif result["label"] == "no_issue" and result["expected"] == "regular":
-                    correct += 1
+                if "label" in row:
+                    result["expected_label"] = row.get("label")
+                    if (
+                        result["expected_label"] in ["no_issue", "not_an_issue"]
+                        and result["label"] == "no_issue"
+                    ):
+                        correct += 1
+                    elif (
+                        result["expected_label"] not in ["no_issue", "not_an_issue"]
+                        and result["label"] != "no_issue"
+                    ):
+                        correct += 1
 
     return results, correct / index
 
@@ -191,18 +212,19 @@ if __name__ == "__main__":
 
     parser.add_argument("filename")
     parser.add_argument("--type", choices=["language-validation", "vandalism"])
-    parser.add_argument("--log-dir", default="./logs")
+    parser.add_argument("--out", default="./output")
 
     args = parser.parse_args()
 
-    pathlib.Path(args.log_dir).mkdir(parents=True, exist_ok=True)
+    pathlib.Path(args.out).mkdir(parents=True, exist_ok=True)
 
     t0 = time.time()
-    results, accuracy = evaluate_prompts(args.filename, args.type)
+    results, accuracy = evaluate_prompts(eval_type=args.type, csv_path=args.filename)
     t1 = time.time()
 
-    print(f"Overall Accuracy: {accuracy:.2%}")
-    print(f"Total time (min): {(t1-t0)/60.0:.2}")
-    model_log_path = f"{args.log_dir}/{args.type}.json"
-    with open(model_log_path, "w") as f:
+    if accuracy > 0:
+        print(f"Overall Accuracy: {accuracy:.2%}")
+    print(f"Total time (sec): {(t1-t0):.2}")
+    model_results_path = f"{args.out}/{args.type}.json"
+    with open(model_results_path, "w") as f:
         json.dump(results, f, indent=4)
