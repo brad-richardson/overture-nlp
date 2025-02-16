3b3f0cc412369ed55c541c32f1ce2f1559e4b070
Brad Richardson <bradrichardsonmail@gmail.com>
2025-02-16 09:16:43 -0500

    Updates to cache gpu layers, fix params

--- example-llama-server.sh
+++ example-llama-server.sh
@@ -1,7 +1,7 @@
 # This is for running locally without docker or on unsupported platforms for docker (e.g. macOS)
-# Download binaries here: https://github.com/ggml-org/llama.cpp/releases
+# Download binary here: https://github.com/ggml-org/llama.cpp/releases, then unzip to match directory structure
 
 MODEL_FILENAME="${MODEL_FILENAME:-Meta-Llama-3.1-8B-Instruct-Q4_K_M.gguf}"
 echo "Running with model at ./${MODEL_FILENAME}"
 
-./llama-cpp-bin/llama-server --model ./models/$MODEL_FILENAME --ctx-size 8192 --parallel 4
+./build/bin/llama-server --model ./models/$MODEL_FILENAME --ctx-size 16384 --parallel 8 --n-gpu-layers 20
